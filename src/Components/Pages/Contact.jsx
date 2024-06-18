cd23023ddcc9445f53d2153e1006d1253eec8011
Samiroutlook <samirmc8384@outlook.com>
2024-06-18 14:32:11 +0600

    about full completed

--- src/Components/Pages/Contact.jsx
+++ src/Components/Pages/Contact.jsx
@@ -1,22 +1,63 @@
 import React from "react";
-import { Grid } from "@chakra-ui/react";
+import {
+  Grid,
+  Button,
+  Input,
+  Stack,
+  Flex,
+  Textarea,
+  Box,
+  useColorModeValue,
+} from "@chakra-ui/react";
 import { motion } from "framer-motion";
 
 function Contact() {
+  const transparent = useColorModeValue("transparent.502", "transparent.50");
+
   return (
     <motion.div
       initial={{ opacity: 0 }}
       animate={{ opacity: 1 }}
       transition={{ duration: 0.5, ease: "easeOut" }}
     >
-      <Grid
-        placeItems={"center"}
+      <Flex
+        w={"full"}
         h={"100vh"}
-        fontSize={["2xl", "3xl", "3xl"]}
-        fontWeight={600}
+        justifyContent={"center"}
+        alignItems={"center"}
       >
-        Hello, I am Contact
-      </Grid>
+        <Stack spacing={3} w={"450px"} px={4}>
+          <Input
+            w={"full"}
+            sx={{ transition: "all 0.1s ease-in-out" }}
+            variant="flushed"
+            placeholder="Name"
+          />
+          <Input
+            w={"full"}
+            sx={{ transition: "all 0.1s ease-in-out" }}
+            variant="flushed"
+            placeholder="Address"
+          />
+          <Input
+            w={"full"}
+            sx={{ transition: "all 0.1s ease-in-out" }}
+            variant="flushed"
+            placeholder="Phone-Number"
+          />
+          <Textarea
+            w={"full"}
+            sx={{ transition: "all 0.1s ease-in-out" }}
+            variant="filled"
+            h={"20vh"}
+            textAlign={"top"}
+            placeholder="Enter your massage"
+          />
+          <Button w={"90px"} mx={"auto"}>
+            Submit
+          </Button>
+        </Stack>
+      </Flex>
     </motion.div>
   );
 }
